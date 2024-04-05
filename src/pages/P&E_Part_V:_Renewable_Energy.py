06afa77c0808d16b847956222eda734699099b86
Anthony Vazquez <vazquez01@allegheny.edu>
2024-04-05 14:04:06 -0400

    Possible fix

--- src/pages/P&E_Part_V:_Renewable_Energy.py
+++ src/pages/P&E_Part_V:_Renewable_Energy.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 from streamlit_lottie import st_lottie
 
-# Creating sidebar message to prompting user to choose a directory
+# Creating sidebar message to prompting user to choose a directory 
 st.sidebar.success("Choose a page from directory above.")
 
 # Function to load Lottie File animations
